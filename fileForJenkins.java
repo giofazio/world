05494316d269e9e05fe71d7bde173b5e052f75b5
giofazio <U.Falcomata.esterno@almaviva.it>
2020-05-25 17:48:57 +0200

    commit per merge da develop a jenkins e jenkingWeb

--- fileForJenkins.java
+++ fileForJenkins.java
@@ -1,8 +1,10 @@
 /**
 * Classe modificata da Umberto su develop
 * aggiunto surname per secondo merge da develop a jenkinsWeb
+* aggiunto address per terzo merge da developa jenkins e jenkinsWeb
 */
 class fileForJenkins {
 	private String name;
 	private String surname;
+	private String address;
 }
