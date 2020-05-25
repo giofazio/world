cce3b32bf131296b35d95ad0fe70596f832a4075
giofazio <U.Falcomata.esterno@almaviva.it>
2020-05-25 17:21:29 +0200

    Seconda prova di merge da develop a jenkinsWeb

--- fileForJenkins.java
+++ fileForJenkins.java
@@ -1,6 +1,8 @@
 /**
 * Classe modificata da Umberto su develop
+* aggiunto surname per secondo merge da develop a jenkinsWeb
 */
 class fileForJenkins {
 	private String name;
+	private String surname;
 }
